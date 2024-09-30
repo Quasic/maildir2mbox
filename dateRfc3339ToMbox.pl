cab52bdbae3881849fd252548200dc8e3d73596d
Quasic <qbasic010@aim.com>
2024-09-30 05:25:42 -0400

    Refactored to eliminate the following:
    1. The separate dateRfc3339ToMbox.pl
       required editing the previous script on some systems
       so I shortened and inlined it.
    2. Date errors from old issue (resolves #2)
       mitigated by falling back to:
       a. loose pattern matching in the perl script
       b. date command
       c. last resort hardcoded midnight, first day of 1900
          assuming obvious wrong date
          with note placed at email end.
    3. echo is not found on some systems
       so I replaced it with printf.
    4. The temp file was replaced with a subshell,
       which also avoids clobbering variables in a pipe,
       as well as providing better parallelization
       and allowing use in a read-only file system.
    5. Some shellcheck flags.

--- dateRfc3339ToMbox.pl
+++ dateRfc3339ToMbox.pl
@@ -3,17 +3,11 @@
 use strict;
 use warnings;
 
-use DateTime::Format::Mail;
-
-my $ORIGINAL = $ARGV[0];
-
-my $RES;
-
-my $parser = DateTime::Format::Mail->new->loose;
-
-#my $datetime = DateTime::Format::Mail->parse_datetime($ORIGINAL);
-my $datetime = $parser->parse_datetime($ORIGINAL);
-
-$RES = $datetime->strftime("%a %b %e %T %Y");
-
-print $RES;
+eval {
+	require DateTime::Format::Mail;
+	print DateTime::Format::Mail->new->loose->parse_datetime($ARGV[0])->strftime("%a %b %e %T %Y");
+};
+if($@){
+	$ARGV[0]=~/(Sun|Mon|Tue|Wed|Thu|Fri|Sat),? ([0-3]?[0-9]) (Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec) ([0-9]+) ([0-2]?[0-9]:[0-5]?[0-9]:[0-5]?[0-9])/ or die;
+	print "$1 $3 $2 $5 $4";
+}
